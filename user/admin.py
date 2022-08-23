fb6859ac39ce3cd5f23217703ca3d436ec2c81ec
AltynaiMaimekova <janarbek.akinai@gmail.com>
2022-08-23 22:27:26 +0600

    admin adjusted

--- user/admin.py
+++ user/admin.py
@@ -6,33 +6,40 @@ from .models import *
 @admin.register(Mentor)
 class MentorAdmin(admin.ModelAdmin):
 
-    # @admin.display(description='общее кол-во квартир')
-    # def total_apartments(self, obj):
-    #     return obj.number_apartments * obj.floors
-    #
-    # @admin.display(description='итоговая стоимость всех квартир')
-    # def total_cost_all_apartments(self, obj):
-    #     total_cost = 0
-    #     for apartment in obj.apartment_set.all():
-    #         total_cost += apartment.squire * obj.cost_per_sqm
-    #     return total_cost
+    @admin.display(description='level')
+    def experience_level(self, obj):
+        if obj.experience > 3:
+            level = 'middle'
+        else:
+            level = 'strong junior'
+        return level
+
+    @admin.display(description='student_list')
+    def student_list(self, obj):
+        student_list = []
+        for student in obj.students.all():
+            info = {'name': student.name, 'job': student.work_study_place, 'phone': student.phone_number}
+            student_list.append(info)
+        return student_list
 
     list_display = ('name', 'email', 'phone_number', 'main_work',
-                    'experience')
+                    'experience', 'experience_level', 'student_list')
 
     search_fields = ['name',]
 
-# @admin.register(Apartment)
-# class ApartmentAdmin(admin.ModelAdmin):
-#
-#     @admin.display(description='итоговая стоимость')
-#     def total_price(self, obj):
-#         return obj.squire * obj.block.cost_per_sqm
-#
-#     date_hierarchy = 'date_purchase'
-#     list_display = ('surname', 'date_purchase', 'status', 'squire', 'total_price')
-#     list_editable = ('date_purchase',)
-#     empty_value_display = '--без хоз--'
-#     list_filter = ('block', 'status')
-#     search_fields = ['surname',]
+
+@admin.register(Course)
+class CourseAdmin(admin.ModelAdmin):
+
+    @admin.display(description='student name')
+    def student_name(self, obj):
+        return obj.student.name
+
+    @admin.display(description='mentor name')
+    def mentor_name(self, obj):
+        return obj.mentor.name
+
+    list_display = ('name', 'date_started', 'language', 'student_name', 'mentor_name')
+    list_filter = ('language', 'mentor')
+    search_fields = ['student__name', 'mentor__name']
 
